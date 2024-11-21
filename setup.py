73d906dd609ab3606ffd61aad9b56f100f3951ad
Amil Khan <amil@ucsb.edu>
2024-11-21 15:05:42 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup, find_packages
 
 
-# Read the README file
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name='bqapi-ucsb',
     version='1.0.2',
     author="Bisque Team",
     author_email='amil@ucsb.edu',
     description="""Python API for interacting with BisQue""",
-    long_description=long_description,
-    long_description_content_type="text/markdown", 
     packages=find_packages('src'),
     package_dir={'': 'src'},
     keywords='API Bisque',
