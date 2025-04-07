4e746bacb88fd5c4b55ecf0ee74eeeb2ea9234c8
Amil Khan <amil@ucsb.edu>
2025-04-07 12:50:03 -0700

    Bug fix, bump version v1.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bqapi-ucsb',
-    version='1.0.2',
+    version='1.0.3',
     author="Bisque Team",
     author_email='amil@ucsb.edu',
     description="""Python API for interacting with BisQue""",
